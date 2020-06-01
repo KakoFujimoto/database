a78b500ff1e19f9694f55811c4e8cccc662f12a3
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-06-01 16:06:13 +0900

    db-014.sqlを更新

--- db/db-challenge3/db-014/db-014.sql
+++ db/db-challenge3/db-014/db-014.sql
@@ -1,8 +1,18 @@
 begin;
+
+delete cm from chat_members cm
+left join chat_rooms as c
+on cm.chat_id = c.id
+left join  users as u
+on cm.user_id = u.id
+where u.is_deleted =1;
+
 delete c from chat_rooms c
 left join chat_members as cm
 on c.id = cm.chat_id
 left join  users as u
 on cm.user_id = u.id
 where u.is_deleted =1;
+
+
 commit;
