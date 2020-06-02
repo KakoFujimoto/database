b36337ae9d1712d91aa51004a454207c5b40b8cb
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-06-02 18:23:32 +0900

    db-010.sql文末にセミコロンを追加

--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -2,4 +2,4 @@ select u.name as '最新の投稿をしたユーザー名' , p.newest_posts as '
 left join (select user_id, created_at, min(created_at) as newest_posts from posts) as p on u.id = p.user_id 
 left join (select id, name, create_user_id from chat_rooms) as c on u.id = c.create_user_id
 group by c.id having min(p.created_at)
-order by c.id
+order by c.id;
