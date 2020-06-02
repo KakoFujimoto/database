0e7d30b064ff5ac60ca1f29193e24ce18a5fbe58
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-06-02 18:26:37 +0900

    db-007.sql文末のセミコロンとid昇順のorder句を追加

--- db/db-challenge3/db-007/db-007.sql
+++ db/db-challenge3/db-007/db-007.sql
@@ -4,3 +4,4 @@ when 0 then '許可'
 when 1 then '禁止' end,
 is_directed, is_deleted, create_user_id, created_at, update_user_id, updated_at 
 from chat_rooms where detail like '%ダイレクトチャット' and is_deleted = 0
+order by id;
