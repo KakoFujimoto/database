925621f4016229e96b507ef370856f5d9a2f60fa
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-06-02 18:34:41 +0900

    db-009.sql文末にセミコロンを追加

--- db/db-challenge3/db-009/db-009.sql
+++ db/db-challenge3/db-009/db-009.sql
@@ -3,4 +3,4 @@ left join (select id, name from chat_rooms) as c on p.chat_id = c.id
 left join (select id, is_deleted from users) as u on p.user_id = u.id
 where p.is_deleted = 0 and u.is_deleted = 0
 group by chat_id
-order by count(p.id) desc
+order by count(p.id) desc;
