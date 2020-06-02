bdcf25986273f0cf59e9499759472a58e34854d8
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-06-02 18:30:59 +0900

    db-006.sqlにorder by句を追加

--- db/db-challenge3/db-006/db-006.sql
+++ db/db-challenge3/db-006/db-006.sql
@@ -1,1 +1,3 @@
-select id, email, password, name, self_intro, company_phone, cell_phone, is_deleted, create_user_id, created_at, update_user_id, updated_at from users;
+select id, email, password, name, self_intro, company_phone, cell_phone,
+ is_deleted, create_user_id, created_at, update_user_id, updated_at from users
+ order by id;
