4ac51eaa0e2437389ea7f5ef9e2283d54fe30720
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-06-03 16:48:05 +0900

    チャットルームidをselect対象から除外

--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -1,5 +1,5 @@
-select u.name as '最新の投稿をしたユーザー名' , p.newest_posts as '投稿日時', c.name as 'チャットルーム名' , c.id as 'チャットルームid' from users as u 
-left join (select user_id, created_at, min(created_at) as newest_posts from posts) as p on u.id = p.user_id 
+select u.name as '最新の投稿をしたユーザー名' , p.newest_posts as '投稿日時', c.name as 'チャットルーム名' from users as u 
+left join (select user_id, created_at, max(created_at) as newest_posts from posts) as p on u.id = p.user_id 
 left join (select id, name, create_user_id from chat_rooms) as c on u.id = c.create_user_id
-group by c.id having min(p.created_at)
+group by c.id having max(p.created_at)
 order by c.id;
