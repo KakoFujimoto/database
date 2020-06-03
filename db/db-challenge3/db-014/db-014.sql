92f3229bc73bc711b800e6fdbe7ddf73263cffd9
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-06-03 19:13:51 +0900

    db-014.sql,db-014.pngの更新、不要な画像の削除

--- db/db-challenge3/db-014/db-014.sql
+++ db/db-challenge3/db-014/db-014.sql
@@ -1,18 +1,5 @@
 begin;
-
-delete cm from chat_members cm
-left join chat_rooms as c
-on cm.chat_id = c.id
-left join  users as u
-on cm.user_id = u.id
-where u.is_deleted =1;
-
-delete c from chat_rooms c
-left join chat_members as cm
-on c.id = cm.chat_id
-left join  users as u
-on cm.user_id = u.id
-where u.is_deleted =1;
-
-
+SET FOREIGN_KEY_CHECKS = 0;
+delete from chat_members
+where user_id = (select id from users where is_deleted = 1 );
 commit;
