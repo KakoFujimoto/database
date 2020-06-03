7bf9564a4ad8a18e34c23a16e571c28013a176f2
Kako Fujimoto <ayaka20111005@yahoo.co.jp>
2020-06-03 17:34:33 +0900

    ascを除去

--- db/db-challenge3/db-008/db-008.sql
+++ db/db-challenge3/db-008/db-008.sql
@@ -4,4 +4,4 @@ join (select id, name from users where is_deleted = 0) as u
 on cm.user_id = u.id
 join (select id, name from chat_rooms where is_deleted = 0) as c
 on cm.chat_id = c.id
-order by join_time asc;
+order by join_time;
